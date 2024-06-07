bfbc7ccd5d335f6c76f4546662f171754f943646
jayesh <jayeshkausalamitron@gmail.com>
2024-06-08 00:09:53 +0530

    latest code added

--- controllers/barcodeController.js
+++ controllers/barcodeController.js
@@ -4,11 +4,6 @@ const { logger } = require('../utils/logger');
 exports.scanBarcodes = async (req, res, next) => {
     const { image } = req.body;
 
-    if (!image) {
-        logger.error('No image provided');
-        return res.status(400).json({ error: 'No image provided' });
-    }
-
     try {
         const barcodes = await annotateImage(image);
         res.json({ barcodes });
--- routes/barcodeRoutes.js
+++ routes/barcodeRoutes.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const { check } = require('express-validator');
 const { scanBarcodes } = require('../controllers/barcodeController');
+const validateRequest = require('../middlewares/validateRequest');
 
 const router = express.Router();
 
-router.post('/', scanBarcodes);
+router.post(
+    '/',
+    [
+        check('image')
+            .notEmpty()
+            .withMessage('Image is required')
+            .isBase64()
+            .withMessage('Image must be a valid Base64 string'),
+    ],
+    validateRequest,
+    scanBarcodes
+);
 
 module.exports = router;
